5a2375e4ef5d486534fe46145ff8293eb36b741b
Jorge <engjorgedantas@gmail.com>
2025-08-04 19:21:25 -0300

    refactor: organize exports and remove unused comment
    This commit reorders exports in `internationalization/lib/internationalization.dart` and `core/lib/core.dart` for better organization.
    
    It also removes an unused comment from `design_system/lib/design_system.dart` and adds a trailing comma in `easy_task_app/lib/main.dart` for consistent formatting.

--- internationalization/lib/internationalization.dart
+++ internationalization/lib/internationalization.dart
@@ -1,6 +1,6 @@
 library;
 
 export 'package:flutter_localizations/flutter_localizations.dart';
+export 'package:intl/intl.dart';
 
 export './src/generated/l10n.dart';
-export 'package:intl/intl.dart';
